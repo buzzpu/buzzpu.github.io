608b93b5a470967ab620147e142f8c57da82bc91
Buzz <buzz.pu@gmail.com>
2016-01-09 15:03:51 +0800

    將DB改到本地端（foodclub-1）

--- booklog/javascripts/app.js
+++ booklog/javascripts/app.js
@@ -9,7 +9,7 @@ var app = app || {};
 **/
 app.Message = Backbone.Model.extend({
     url: function() {
-         return 'http://launchergap.org/1/post' 
+         return 'http://localhost:3000/1/post' 
                     + ( this.id === null ? '' : '/' + this.id );
     },
     id: null,
@@ -17,8 +17,8 @@ app.Message = Backbone.Model.extend({
         success: false,
         errfor: {},
         posts: [],
-        subject: '',
-        content: ''
+        title: '',
+        message: ''
     }
 });
 
